daab6bab8f86fb3710fe112b9f85a56c91f69afe
Yerkebulan <yerkebulan014@gmail.com>
2024-11-01 15:28:24 +0100

    Added delete button

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from myapp.views import index,indexItem
+from myapp.views import index,indexItem,add_item, update_item, delete_item
 
 app_name = "myapp"
 
 urlpatterns = [
    # http://localhost:8000/myapp/
-    path('', index),
+   path('', index,name="index"),
     # http://localhost:8000/myapp/hello/
-     path('<int:my_id>/', indexItem, name="detail")
+   path('<int:my_id>/', indexItem, name="detail"),
+   path('additem/', add_item, name="add_item"),
+   path('updateitem/<int:my_id>', update_item, name="update_item"),
+   path('deleteitem/<int:my_id>', delete_item, name="delete_item"),
 ]
   
